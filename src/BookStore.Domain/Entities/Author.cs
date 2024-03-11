c576dfc216711df611df027b0188c6377412fcea
BoburTurgunboyev <turgunboyevbbobur7881@mail.ru>
2024-03-12 00:45:11 +0500

    entites qoshildi

---
+++ src/BookStore.Domain/Entities/Author.cs
@@ -0,0 +1,10 @@
+﻿namespace BookStore.Domain.Entities
+{
+    public class Author
+    {
+        public int Id { get; set; }
+        public string FullName { get; set; }
+        public IList<Book> Books { get; set; }
+
+    }
+}
---
+++ src/BookStore.Domain/Entities/Book.cs
@@ -0,0 +1,14 @@
+﻿namespace BookStore.Domain.Entities
+{
+    public class Book
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Price { get; set; }  
+        public int AuthorId { get; set; }
+        public Author Author { get; set; }
+        public int GenreId { get; set; }
+        public Genre Genre { get; set; }
+
+    }
+}
