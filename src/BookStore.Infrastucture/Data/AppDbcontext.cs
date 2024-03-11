5a2f319e35715e69ba63df9b4cfab11cd51358d2
BoburTurgunboyev <turgunboyevbbobur7881@mail.ru>
2024-03-12 00:46:57 +0500

    data qoshildi

---
+++ src/BookStore.Infrastucture/Data/AppDbcontext.cs
@@ -0,0 +1,17 @@
+﻿using BookStore.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace BookStore.Infrastructure.Data
+{
+    public class AppDbcontext : DbContext
+    {
+        public AppDbcontext(DbContextOptions<AppDbcontext> options) : base(options)
+        {
+            Database.Migrate();
+        }
+
+        public DbSet<Author> Authors { get; set; }
+        public DbSet<Book> Books { get; set; }
+        public DbSet<Genre> Genres { get; set; }
+    }
+}
